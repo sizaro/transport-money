349410bfe4b10b0fcbaa696e2feb298b4d9009a9
RONALD BAMUTESIZA <spartansiza@gmail.com>
2026-09-06 02:33:24 +0300

    feat: establish NestJS API foundation

---
+++ apps/api/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module.js'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  })
+
+  await app.listen(3000)
+}
+
+bootstrap()
